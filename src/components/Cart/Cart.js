ed1745c66e7f75515cdd79d514c3aea257a05e9e
Anik Sarker <aniksarker1612@gmail.com>
2020-08-23 23:08:01 +0800

    site ready

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+
+    // const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        total = total + product.price;
+    }
+
+    let shipping = 0;
+    if (total > 35) {
+        shipping = 0;
+    } else if (total > 15) {
+        shipping = 4.99;
+    } else if (total > 0) {
+        shipping = 12.99;
+    }
+
+    const tax = (total * 0.10).toFixed(2);
+    const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+    return (
+        <div className="">
+            <h4>Order Summary</h4>
+            <p>Items ordered: {cart.length}</p>
+            <p>Product Price: {total.toFixed(2)}</p>
+            <p><small>Shipping cost: {shipping}</small></p>
+            <p>Tax + Vat: {tax}</p>
+            <p>Total Price: {grandTotal}</p>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,21 +2,27 @@ import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css'
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProduct] = useState(first10);
+    const [cart, setCart] = useState([]);
 
+    const handleAddProduct = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
 
     return (
         <div className='shop-container'>
             <div className="product-container">
                 {
-                    products.map(product => <Product product={product}></Product>)
+                    products.map(product => <Product handleAddProduct = {handleAddProduct} product={product}></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h4>This is cart</h4>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
